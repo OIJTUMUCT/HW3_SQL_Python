f6a8537ae24279887a98058bacac8402e50f73b0
OIJTUMUCT <gogcomup@gmail.com>
2025-03-05 15:41:24 +0300

    Update fix

--- sql/4_2.sql
+++ sql/4_2.sql
@@ -4,16 +4,14 @@ WITH cte AS (
         SUM(t.list_price) OVER (PARTITION BY t.customer_id) AS total_price,
         MAX(t.list_price) OVER (PARTITION BY t.customer_id) AS max_price,
         MIN(t.list_price) OVER (PARTITION BY t.customer_id) AS min_price,
-        COUNT(*)       OVER (PARTITION BY t.customer_id) AS txn_count,
-        ROW_NUMBER()   OVER (PARTITION BY t.customer_id ORDER BY t.customer_id) AS rn
+        COUNT(*)       OVER (PARTITION BY t.customer_id) AS txn_count
     FROM transaction t
 )
-SELECT
+SELECT 
     customer_id,
     total_price,
     max_price,
     min_price,
     txn_count
 FROM cte
-WHERE rn = 1
 ORDER BY total_price DESC, txn_count DESC;
